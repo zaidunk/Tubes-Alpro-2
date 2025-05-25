38f35c583b9d5883143e4e66006e6cfdb3744813
zaidunk <zaidanberkuliah@gmail.com>
2025-05-25 17:33:06 +0700

    Update RecordData.go
    Hal yang dibuat:
    -Fitur untuk input, edit, hapus data latihan
    -Menambahkan fitur jadwal

--- cara-kerja/RecordData.go
+++ cara-kerja/RecordData.go
@@ -6,90 +6,172 @@ import (
 	"time"
 )
 
-const NMAX int = 100
+const NMAX int = 999
 
 type latihan struct {
 	nama    string
+	jenis   int
 	tanggal time.Time
 	durasi  time.Duration
 	jKalori int
 }
 
+type counter struct {
+	lat         int
+	cardio      int
+	strength    int
+	flexibility int
+}
+
 type tabLatihan [NMAX]latihan
+type tabJadwal [7]int
 
-func Record() {
-	var A tabLatihan
-	var counter int
-	RecordProcess(&A, counter)
-}
+var A tabLatihan
+var B tabJadwal
+var c counter
 
-func RecordMenu() {
-	length := 40
-	t1 := "Workout"
-	sp1 := (length - len(t1)) / 2
-
-	f.Println("+" + s.Repeat("-", length) + "+")
-	f.Println("|" + s.Repeat(" ", sp1) + t1 + s.Repeat(" ", sp1) + "|")
-	f.Println("+" + s.Repeat("-", length) + "+")
-
-	f.Printf("1. Latihan\n2. Durasi\n3. Jumlah Kalori\n4. Submit\n\n")
-}
-
-func RecordDurasi(A *tabLatihan, counter int) {
-	var x string
-	f.Print("Masukkan durasi latihan (contoh: 1h30m17s): ")
+func InputJadwal() {
+	var i, x int
+	CetakHeader("Input Data Jadwal")
+	f.Print("Masukkan hari\n1. Senin\n2. Selasa\n3. Rabu\n4. Kamis\n 5. Jumat\n 6. Sabtu\n 7. Minggu\n\n")
+	f.Print("Pilihanmu? (1/2/3/4/5/6/7): ")
 	f.Scan(&x)
 
-	durasi, err := time.ParseDuration(x)
-	if err != nil {
-		f.Println("Format durasi tidak valid")
-	} else {
-		A[counter].durasi = durasi
+	if x < 1 || x > 7 {
+		f.Println("Index tidak valid.")
+		return
 	}
 
+	i = x - 1
+
+	f.Print("Masukkan jenis latihan\n1. Cardio\n2. Strength\n3. Flexibility\n")
+	f.Print("Pilihanmu? (1/2/3): ")
+	f.Scan(&B[i])
 }
 
-func RecordKalori(A *tabLatihan, counter int) {
-	f.Print("Masukkan jumlah kalori: ")
-	f.Scan(&A[counter].jKalori)
-}
+func HapusJadwal() {
+	var i, x int
+	CetakHeader("Hapus Data Jadwal")
+	f.Print("Masukkan hari\n1. Senin\n2. Selasa\n3. Rabu\n4. Kamis\n 5. Jumat\n 6. Sabtu\n 7. Minggu\n\n")
+	f.Print("Pilihanmu? (1/2/3/4/5/6/7): ")
+	f.Scan(&x)
 
-func RecordSubmit() bool {
-	var num int
-	f.Println("Apakah kamu yakin ingin menyelesaikan workout ini?")
-	f.Printf("1. Yes\n2. No\n\n")
-	f.Print("Pilihanmu? (1/2): ")
-	f.Scan(&num)
-
-	return num == 1
-}
-
-func RecordProcess(A *tabLatihan, counter int) {
-	var num int
-	A[counter].tanggal = time.Now()
-
-	for {
-		RecordMenu()
-		f.Print("Pilihanmu? (1/2/3/4): ")
-		f.Scan(&num)
-
-		switch num {
-		case 1:
-			//DaftarLatihan()
-		case 2:
-			RecordDurasi(A, counter)
-		case 3:
-			RecordKalori(A, counter)
-		case 4:
-			f.Print("Nama workout: ")
-			f.Scan(&A[counter].nama)
-			if RecordSubmit() {
-				f.Println("Workout berhasil disubmit.")
-				//f.Print(A[counter].tanggal)
-				return
-			}
-		default:
-			f.Println("Pilihan tidak valid.")
-		}
+	if x < 1 || x > 7 {
+		f.Println("Index tidak valid.")
+		return
 	}
+
+	i = x - 1
+
+	B[i] = 0
+}
+
+func InputLatihan() {
+	CetakHeader("Input Data Latihan")
+
+	InputSatuLatihan(&A[c.lat])
+	A[c.lat].tanggal = time.Now()
+
+	// Update counter
+	switch A[c.lat].jenis {
+	case 1:
+		c.cardio++
+	case 2:
+		c.strength++
+	case 3:
+		c.flexibility++
+	}
+	c.lat++
+}
+
+func UbahLatihan() {
+	var x, i int
+	CetakHeader("Ubah Data Latihan")
+
+	//Menampilkan riwayat latihan
+	Riwayat()
+	f.Print("Masukkan nomor latihan yang ingin diubah: ")
+	f.Scan(&x)
+
+	if x < 1 || x > c.lat {
+		f.Println("Index tidak valid.")
+		return
+	}
+
+	i = x - 1
+
+	switch A[i].jenis {
+	case 1:
+		c.cardio--
+	case 2:
+		c.strength--
+	case 3:
+		c.flexibility--
+	}
+
+	InputSatuLatihan(&A[i])
+
+	switch A[i].jenis {
+	case 1:
+		c.cardio++
+	case 2:
+		c.strength++
+	case 3:
+		c.flexibility++
+	}
+}
+
+func HapusLatihan() {
+	var x, i int
+	CetakHeader("Hapus Data Latihan")
+	//Menampilkan riwayat latihan
+	Riwayat()
+
+	//Input index latihan yg ingin dihapus
+	f.Scan(&x)
+	if x < 1 || x > c.lat {
+		f.Println("Index tidak valid.")
+		return
+	}
+
+	for i = x - 1; i < c.lat-1; i++ {
+		A[i] = A[i+1]
+	}
+
+	//Update counter
+	switch A[x-1].jenis {
+	case 1:
+		c.cardio--
+	case 2:
+		c.strength--
+	case 3:
+		c.flexibility--
+	}
+	c.lat--
+}
+
+func CetakHeader(text string) {
+	length := 40
+	sp := (length - len(text)) / 2
+	f.Println("+" + s.Repeat("-", length) + "+")
+	f.Println("|" + s.Repeat(" ", sp) + text + s.Repeat(" ", length-len(text)-sp) + "|")
+	f.Println("+" + s.Repeat("-", length) + "+")
+}
+
+func InputSatuLatihan(l *latihan) {
+	var d int
+
+	f.Print("Masukkan nama latihan: ")
+	f.Scan(&l.nama)
+
+	f.Print("Masukkan jenis latihan\n1. Cardio\n2. Strength\n3. Flexibility\n")
+	f.Print("Pilihanmu? (1/2/3): ")
+	f.Scan(&l.jenis)
+
+	f.Print("Masukkan durasi latihan (dalam menit): ")
+	f.Scan(&d)
+	l.durasi = time.Duration(d) * time.Minute
+
+	f.Print("Masukkan jumlah kalori latihan: ")
+	f.Scan(&l.jKalori)
 }
